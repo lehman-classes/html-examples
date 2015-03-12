38b07ae682719e610101257552d4866541dfce36
Hendrix Tavarez <htavarez@gmail.com>
2015-03-12 14:53:03 -0400

    Create scripts2.js

---
+++ scripts2.js
@@ -0,0 +1,9 @@
+function changeBackgroundToRed (hello) {
+	// hello.value="change background to red";
+	hello.style.background = "red";
+}
+
+function changeBackgroundToGreen (hello) {
+	// hello.value="change background to green";	
+	hello.style.background = "green";
+}
