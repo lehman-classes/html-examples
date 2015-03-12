37cdb82a7b692bfa8221c97231deed470d378648
Hendrix Tavarez <htavarez@gmail.com>
2015-03-12 14:52:23 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function mouseover (a) {
+	// alert("mouseover called");
+	// var hello = document.getElementById('hello');
+	a.innerHTML = "Hello JS";
+}
+
+function mouseout (b) {
+	// alert("mouseout called");
+	// var hello = document.getElementById('hello');
+	b.innerHTML = "Hello HTML5";
+}
